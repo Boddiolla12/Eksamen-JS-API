2289b359a2f6dd93a77991bf11f86b747a17cc04
Oskar Gjelstad <oskar.rebard@gokstadakademiet.no>
2024-05-07 03:29:30 +0200

    Implemented logic to fetch pokemon data from api + errorhandling incase fetch fails

---
+++ js/home.js
@@ -0,0 +1,35 @@
+// Define async function
+const fetchPokemon = async () => {
+  const pokeArray = [];
+
+  // Create promises for fetching data for each pokemon
+  for (let i = 1; i < 152; i++) {
+    const baseUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
+
+    try {
+      const response = await fetch(baseUrl);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch pokemon ${i}`);
+      }
+      const data = await response.json();
+      pokeArray.push(data);
+    } catch (error) {
+      // Catching error within the loop, and stops looping if error is encountered
+      console.log(`Error fetching pokemon ${i}`, error);
+      return;
+    }
+  }
+
+  // processing fetched pokedata
+  const pokemon = pokeArray.map((data) => ({
+    name: data.name,
+    id: data.id,
+    image: data.sprites["front_default"], //Uses the api's front default sprite as image
+    type: data.types.map((type) => type.type.name).join(", "), //maps pokemon types into a single string
+  }));
+
+  // Log processed data
+  console.log(pokemon);
+};
+
+fetchPokemon();
