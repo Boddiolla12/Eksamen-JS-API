090549e6a62166298fd613b638e828a23c3928ee
Oskar Gjelstad <oskar.rebard@gokstadakademiet.no>
2024-05-08 16:20:44 +0200

    moved goToDetailsPage function to its own document, so it can be used on both favorites and home page,

--- js/displayPokemon.js
+++ js/displayPokemon.js
@@ -22,8 +22,3 @@ const displayPokemon = (pokemonData) => {
     pokemonContainer.appendChild(pokemonElement);
   });
 };
-
-//function to take user to details page when button is clicked
-const goToDetailsPage = (pokemonId) => {
-  window.location.href = `details.html?id=${pokemonId}`;
-};
---
+++ js/goToDetailsPage.js
@@ -0,0 +1,4 @@
+//function to take user to details page when button is clicked
+const goToDetailsPage = (pokemonId) => {
+  window.location.href = `details.html?id=${pokemonId}`;
+};
