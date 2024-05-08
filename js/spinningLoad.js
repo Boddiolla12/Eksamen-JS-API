d906139f02261964638fc32bca76afe1940e6571
Oskar Gjelstad <oskar.rebard@gokstadakademiet.no>
2024-05-08 16:29:22 +0200

    separated spinningload functionality into two js files to prevent errors when used in index.html

--- js/spinningLoad.js
+++ js/spinningLoad.js
@@ -9,38 +9,3 @@ const hideSpinner = () => {
   const spinner = document.querySelector(".spinner");
   spinner.style.display = "none";
 };
-
-//Check if the user is already logged in on pageload
-window.addEventListener("load", () => {
-  if (checkLoggedIn()) {
-    //hide spinner
-    setTimeout(() => {
-      hideSpinner();
-      // show logout button
-      document.getElementById("logoutBtn").style.display = "block";
-    }, 300);
-
-    // Hide login form elements
-    const elementsToHide = [
-      "username",
-      "usernameLabel",
-      "password",
-      "passwordLabel",
-      "registerLabel",
-      "registerBtn",
-      "loginBtn",
-    ];
-    toggleElementDisplay(elementsToHide, "none");
-  }
-});
-
-// Hide the form wrapper initially
-document.getElementById("formWrapper").style.display = "none";
-
-//show spinner while waiting for form
-// Delay displaying the form after 500 milliseconds (adjust the delay as needed)
-setTimeout(() => {
-  showSpinner();
-  document.getElementById("formWrapper").style.display = "block";
-  hideSpinner();
-}, 500);
