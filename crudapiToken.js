efc5e4506ef27f63ef59486c4a7cf558a68404db
Oskar Gjelstad <oskar.rebard@gokstadakademiet.no>
2024-05-18 03:30:46 +0200

    Removed redundant file

--- crudapiToken.js
+++
@@ -1,9 +0,0 @@
-const crudApiKey = "Eb5KYt8dM3e2LyocFBBcIHdhbbHUWVQs6xl6TuRSWFAVSL6i1g";
-
-/*
-const crudApiKey = Eb5KYt8dM3e2LyocFBBcIHdhbbHUWVQs6xl6TuRSWFAVSL6i1g;
-const crudApiKey = Eb5KYt8dM3e2LyocFBBcIHdhbbHUWVQs6xl6TuRSWFAVSL6i1g;
-const crudApiKey = Eb5KYt8dM3e2LyocFBBcIHdhbbHUWVQs6xl6TuRSWFAVSL6i1g;
-const crudApiKey = Eb5KYt8dM3e2LyocFBBcIHdhbbHUWVQs6xl6TuRSWFAVSL6i1g;
-
-*/
