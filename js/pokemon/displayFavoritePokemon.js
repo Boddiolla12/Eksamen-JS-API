8d7f59d36e37a4d13d6c07a2a84f51f25c2ec193
Oskar Gjelstad <oskar.rebard@gokstadakademiet.no>
2024-05-18 03:16:40 +0200

    Fixed styling, and removed setTimeout for favorite page

--- js/pokemon/displayFavoritePokemon.js
+++ js/pokemon/displayFavoritePokemon.js
@@ -30,26 +30,17 @@ const displayFavoritePokemon = async () => {
     // Get array of favorited pokemon ID's
     const favoritePokemonIds = userData.favorites;
 
-    // wait for set duration to ensure spinner is displayed
-    await new Promise((resolve) => setTimeout(resolve, 300));
-
     // get container where savedPokemon will be displayed
-    const favoritePokemonContainer = document.getElementById(
-      "favoritePokemon-container"
-    );
+    const favoritePokemonContainer = document.getElementById("favoritePokemon-container");
 
     //Clear previous content
     favoritePokemonContainer.innerHTML = "";
 
     //add class to container
-    favoritePokemonContainer.classList.add(
-      "favoritePokemon-container",
-      "pokemon-container"
-    );
+    favoritePokemonContainer.classList.add("favoritePokemon-container", "pokemon-container");
 
     //retrieve existing favoritePokemon IDs from local storage
-    let existingFavoritePokemonIds =
-      JSON.parse(localStorage.getItem("favoritePokemonIds")) || [];
+    let existingFavoritePokemonIds = JSON.parse(localStorage.getItem("favoritePokemonIds")) || [];
 
     //check if favorite pokemon IDs already exist in local storage, if not add them
     favoritePokemonIds.forEach((pokemonId) => {
@@ -59,35 +50,27 @@ const displayFavoritePokemon = async () => {
     });
 
     //Save updated favorite Pokemon IDS to local storage
-    localStorage.setItem(
-      "favoritePokemonIds",
-      JSON.stringify(existingFavoritePokemonIds)
-    );
+    localStorage.setItem("favoritePokemonIds", JSON.stringify(existingFavoritePokemonIds));
 
     //Fetch data for each saved pokemonID and display them after all have been fetched
-    const pokemonPromises = existingFavoritePokemonIds.map(
-      async (pokemonId) => {
-        try {
-          const getResponse = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
+    const pokemonPromises = existingFavoritePokemonIds.map(async (pokemonId) => {
+      try {
+        const getResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-          if (!getResponse.ok) {
-            throw new Error(`Failed to fetch pokemon ${pokemonId}`);
-          }
-
-          return getResponse.json();
-        } catch (error) {
-          console.error("Error fetching or displaying pokemon data:", error);
+        if (!getResponse.ok) {
+          throw new Error(`Failed to fetch pokemon ${pokemonId}`);
         }
+
+        return getResponse.json();
+      } catch (error) {
+        console.error("Error fetching or displaying pokemon data:", error);
       }
-    );
+    });
 
     //Wait for all pokemon data to be fetched
     const pokemonData = await Promise.all(pokemonPromises);
@@ -107,12 +90,8 @@ const displayFavoritePokemon = async () => {
       <h2>${pokemon.name}</h2>
       <p># ${pokemon.id}</p>
       <p>Type: ${pokemon.types.map((type) => type.type.name).join(", ")}</p>
-      <button class="removeBtn" onclick="removeFavoritePokemon(${
-        pokemon.id
-      })">Remove</button>
-      <button class="detailsBtn"  onclick="goToDetailsPage(${
-        pokemon.id
-      })">Details</button>
+      <button class="removeBtn" onclick="removeFavoritePokemon(${pokemon.id})">Remove</button>
+      <button class="detailsBtn"  onclick="goToDetailsPage(${pokemon.id})">Details</button>
       `;
 
       //append pokemon element to favorite container
@@ -122,10 +101,7 @@ const displayFavoritePokemon = async () => {
     //hide spinner after all pokemondata has been fetched and displayed
     hideSpinner();
   } catch (error) {
-    console.error(
-      "Error displaying saved pokemon, user might not be logged in",
-      error
-    );
+    console.error("Error displaying saved pokemon, user might not be logged in", error);
   } finally {
     hideSpinner();
 
